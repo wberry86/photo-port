f9fb2cf676f156089d6893f79ee3fca0f1bb1f41
William Berry <76190311+wberry86@users.noreply.github.com>
2021-06-22 08:04:37 -0600

    fixed css issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-import './App.css';
 
 function App() {
+
   return (
     <div>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
     </div>
   );
